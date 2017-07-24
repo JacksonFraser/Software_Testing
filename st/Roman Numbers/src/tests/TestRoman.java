70785daa766bb908ad8cc804ea1589e9dd0b6c9a
jacksonfraser <s3529589@student.rmit.edu.au>
2017-07-24 21:30:59 +1000

    Changes

--- st/Roman Numbers/src/tests/TestRoman.java
+++ st/Roman Numbers/src/tests/TestRoman.java
@@ -7,23 +7,33 @@ import org.junit.Test;
 import roman.Roman;
 import roman.RomanException;
 
-public class TestToRomanFunction {
+public class TestRoman {
 
 	@Test(expected = RomanException.class)
-	public void testRomanMinimumValueException() throws RomanException {
+	public void testToRomanMinimumValueException() throws RomanException {
 		Roman.toRoman(0);
 	}
 
 	@Test(expected = RomanException.class)
-	public void testRomanMaximumValueException() throws RomanException {
+	public void testToRomanMaximumValueException() throws RomanException {
 		Roman.toRoman(39000);
+	}
+
+	@Test(expected = RomanException.class)
+	public void testFromRomanMinimumValueException() throws RomanException {
+		Roman.fromRoman("");
+	}
+
+	@Test(expected = RomanException.class)
+	public void testFromRomanMaximumValueException() throws RomanException {
+		String max = Roman.toRoman(39000);
+		Roman.fromRoman(max);
 	}
 
 	@Test
 	public void testValidValuesAccepted() {
 		for (int i = 1; i < 39000; ++i) {
 			try {
-
 				String romanString = Roman.toRoman(i);
 				int romanInt = Roman.fromRoman(romanString);
 				assertEquals(i, romanInt);
